f4d7ce4c2e8c178b4c2594ec5c82834d4d19c5a9
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-01-20 11:24:06 -0300

    create a layout for mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
+import "./src/lib/dayjs";
 export default function App() {
   const [fontsLoad] = useFonts({
     Inter_400Regular,
@@ -12,22 +14,9 @@ export default function App() {
   if (!fontsLoad) return (<Loading />);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+    <>
+      <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090A",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text:{
-    color:"#FFF",
-    fontFamily:"Inter_800ExtraBold"
-  }
-});
