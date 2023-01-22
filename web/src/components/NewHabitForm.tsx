83b85423a774656077c4b41eb21cdd3396949025
Pedro Apolinario <62606780+PedroLuizAP@users.noreply.github.com>
2023-01-22 12:10:10 -0300

    finish web

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -3,7 +3,15 @@ import * as Checkbox from '@radix-ui/react-checkbox';
 import { FormEvent, useState } from "react";
 import { api } from "../lib/axios";
 
-const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado',]
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+]
 
 export function NewHabitForm() {
 
@@ -30,10 +38,10 @@ export function NewHabitForm() {
 
   function handleToggleWeekDay(weekDay: number) {
     if (weekDays.includes(weekDay)) {
-      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay);
+      const weekDaysWithRemovedOne = weekDays.filter(day => day !== weekDay)
       setWeekDays(weekDaysWithRemovedOne);
     } else {
-      const weekDaysWithAddedOne = [...weekDays, weekDay];
+      const weekDaysWithAddedOne = [...weekDays, weekDay]
       setWeekDays(weekDaysWithAddedOne);
     }
   }
@@ -44,8 +52,8 @@ export function NewHabitForm() {
         Qual seu comprometimento?
       </label>
 
-      <input type="text" id="title" placeholder="ex.: Exercícios, dormir bem, etc..." className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400" autoFocus value={title}
-        onChange={event => setTitle(event.target.value)} />
+      <input type="text" id="title" placeholder="ex.: Exercícios, dormir bem, etc..." autoFocus value={title} onChange={event => setTitle(event.target.value)}
+        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900" />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4">
         Qual a recorrência?
@@ -53,8 +61,8 @@ export function NewHabitForm() {
 
       <div className="flex flex-col gap-2 mt-3">
         {availableWeekDays.map((weekDay, index) => (
-          <Checkbox.Root key={weekDay} className='flex items-center gap-3 group' checked={weekDays.includes(index)} onCheckedChange={() => handleToggleWeekDay(index)} >
-            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50'>
+          <Checkbox.Root key={weekDay} className='flex items-center gap-3 group focus:outline-none' checked={weekDays.includes(index)} onCheckedChange={() => handleToggleWeekDay(index)} >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
               <Checkbox.Indicator>
                 <Check size={20} className="text-white" />
               </Checkbox.Indicator>
@@ -67,7 +75,7 @@ export function NewHabitForm() {
         ))}
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900">
         <Check size={20} weight="bold" />
         Confirmar
       </button>
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -49,13 +49,13 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => {
+        {summary.length && summaryDates.map((date) => {
           const dayInSummary = summary.find(day => {
             return dayjs(date).isSame(day.date, 'day');
           });
 
           return (
-            <HabitDay key={date.toString()} date={date} amount={dayInSummary?.amount} completed={dayInSummary?.completed} />
+            <HabitDay key={date.toString()} date={date} amount={dayInSummary?.amount} defaultCompleted={dayInSummary?.completed} />
           );
         })}
 
